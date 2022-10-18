951cd8226a0ce08f7c2c82eb14bee573da9331b4
Nicolas Miguenz <nmiguenz@soluciones.com.ar>
2022-10-18 02:11:57 -0300

    getters y setters a firebase listo. Deploy a Firebase listo.

---
+++ public/firebase.js
@@ -0,0 +1,28 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-app.js";
+import { getFirestore, collection, getDocs, addDoc } from "https://www.gstatic.com/firebasejs/9.11.0/firebase-firestore.js"
+
+//Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBtmnxB5wWIY1iac0RYFkNbgSLJjyHfoI4",
+  authDomain: "web-deturno.firebaseapp.com",
+  projectId: "web-deturno",
+  storageBucket: "web-deturno.appspot.com",
+  messagingSenderId: "309784192914",
+  appId: "1:309784192914:web:f0d5ced82bd7a68a7e434b",
+  measurementId: "G-174FW38SJE"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+// Get a collection from your database
+export const getCollection = async (nombreColeccion) => {
+  const colRef = collection(db, nombreColeccion)
+  const docs = await getDocs(colRef);
+  return docs;
+}
+
+//Upload to the database
+export const alta = (nombreColeccion, objeto) =>{ addDoc(collection(db,nombreColeccion),objeto);}
--- public/index.js
+++ public/index.js
@@ -1,7 +1,33 @@
+import {getCollection, alta} from './firebase.js';
+
+const formOp = document.querySelector('form'); 
+
+window.addEventListener('DOMContentLoaded', async () => {
+    const querySnapshot = await getCollection('comentarios');
+    
+    querySnapshot.forEach(element => {
+        console.log(element.data());
+    });
+});
+
+
 window.addEventListener('scroll', function(){
     const header = document.querySelector('header');
     header.classList.toggle("move", window.scrollY > 0);
-})
+});
+
+// Alta de comentarios
+window.addEventListener('submit', (e)=>{
+    e.preventDefault();
+
+    const opinion = {
+        nombre: formOp['nombre'].value,
+        apellido: formOp['email'].value,
+        comentario: formOp['comentario'].value
+    };
+
+    alta('comentarios', opinion);
+});
 
 function toggleMenu(){
     const menuToggle = document.querySelector('.menuToggle');
